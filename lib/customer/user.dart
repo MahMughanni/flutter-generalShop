6bc557050f3b9d8e5471313857fa71f356130c55
Mahmoud AlMughanni <mahmoudcr95@gmail.com>
2020-10-09 22:13:13 +0300

    edit UserModel

--- lib/customer/user.dart
+++ lib/customer/user.dart
@@ -3,12 +3,13 @@ class User {
   String last_name;
   String email;
   String api_token; // optional on constructor
-  int user_id;
+  int user_id; // optional on constructor
 
   User(this.first_name, this.last_name, this.email,
       [this.api_token, this.user_id]);
 
   User.fromJson(Map<String, dynamic> jsonObject) {
+
     this.user_id = jsonObject['user_id'];
     this.first_name = jsonObject['first_name'];
     this.last_name = jsonObject['last_name'];
