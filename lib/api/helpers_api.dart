2ae931a453d6fdc8e7509bfc4c6c64c8f3b00eae
Mahmoud AlMughanni <mahmoudcr95@gmail.com>
2020-10-12 03:25:06 +0300

    add models and helpers_api

---
+++ lib/api/helpers_api.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_generalshop/utility/Country.dart';
+
+class HelperAPi {
+  void fetchCategories(int page) {}
+
+  void fetchTags(int page) {}
+
+  void fetchCountries(int page) {}
+
+  void fetchStates(Country country) {}
+
+  void fetchCities(Country country) {}
+}
